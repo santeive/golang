8eb3307c21a65019daed70dcc7fccf86e5b094ec
Francisco Garcia <packo.ive@gmail.com>
2024-04-10 17:45:42 -0600

    Profit calculator and use of functions and formats

--- profitCalculator/main.go
+++ profitCalculator/main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func returnTwoValues() (int, int) {
+	// Multiple value return
+	aVar := 100
+	bVar := 200
+
+	return aVar, bVar
+}
+
+func alternativeReturnTwoValues() (aVar int, bVar int) {
+	// Multiple value return
+	aVar = 1
+	bVar = 1
+
+	// Good but not explicit
+	return
+}
+
 func main() {
 	//** Build a profit calculator **
 	var revenue float64
@@ -9,13 +30,13 @@ func main() {
 	var taxRate float64
 
 	//- Ask for revenue, expenses & tax rate
-	fmt.Print("Revenue: ")
+	outputText("Revenue: ")
 	fmt.Scan(&revenue)
 
-	fmt.Print("Expenses: ")
+	outputText("Expenses: ")
 	fmt.Scan(&expenses)
 
-	fmt.Print("taxRate: ")
+	outputText("taxRate: ")
 	fmt.Scan(&taxRate)
 
 	//- Calculate earnings before tax (EBT) and earnings after tax (profit)
@@ -29,6 +50,8 @@ func main() {
 	fmt.Println("EBT: ", ebt)
 	fmt.Println("profit: ", profit)
 	fmt.Println("ratio: ", ratio)
+
+	fmt.Print(alternativeReturnTwoValues())
 
 	// Create a module
 	// go mod init example.com/basics-practice
