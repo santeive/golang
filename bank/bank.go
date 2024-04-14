fbe79650de0035f26d450f1926bea980ae3ae72e
Francisco Garcia <packo.ive@gmail.com>
2024-04-14 17:35:47 -0600

    If statement

--- bank/bank.go
+++ bank/bank.go
@@ -9,16 +9,54 @@ func menu() {
 	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposit money")
 	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
+	fmt.Println("4. Just a loop")
+	fmt.Println("5. Exit")
 
 	fmt.Println("Select your choice: ")
 }
 
+func justALoopFunction() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("My index: ", i*2+1)
+	}
+}
+
 func main() {
 	var option int
+	var accountBalance = 1000.0
 
 	menu()
 	fmt.Scan(&option)
-
 	fmt.Println("Your choice: ", option)
+
+	if option == 1 {
+		fmt.Println("Your balance is: ", accountBalance)
+	} else if option == 2 {
+		fmt.Println("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		if depositAmount <= 0 {
+			fmt.Println("Invalid amount. must be greater that 0")
+			return
+		}
+		accountBalance += depositAmount
+		fmt.Println("Your new balance is: ", accountBalance)
+	} else if option == 3 {
+		fmt.Println("Your withdraw: ")
+		var amount float64
+		fmt.Scan(&amount)
+
+		if amount >= accountBalance {
+			fmt.Println("Invalid amount. You can't withdraw more than you have.")
+			return
+		}
+
+		accountBalance -= amount
+		fmt.Println("Your new balance is: ", accountBalance)
+	} else if option == 4 {
+		justALoopFunction()
+	} else {
+		fmt.Println("Goodbye")
+		return
+	}
 }
