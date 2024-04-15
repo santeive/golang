460d74c4fcff16256d96acb3e8d6f2b1a9486986
Francisco Garcia <packo.ive@gmail.com>
2024-04-14 18:13:06 -0600

    Bucle infinito

--- bank/bank.go
+++ bank/bank.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func menu() {
+	fmt.Println()
 	fmt.Println("*** Welcome to the bank *** ")
 	fmt.Println("What do you want to do?")
 
@@ -25,38 +26,41 @@ func main() {
 	var option int
 	var accountBalance = 1000.0
 
-	menu()
-	fmt.Scan(&option)
-	fmt.Println("Your choice: ", option)
+	// Bucle infinito
+	for {
+		menu()
+		fmt.Scan(&option)
+		fmt.Println("Your choice: ", option)
 
-	if option == 1 {
-		fmt.Println("Your balance is: ", accountBalance)
-	} else if option == 2 {
-		fmt.Println("Your deposit: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-		if depositAmount <= 0 {
-			fmt.Println("Invalid amount. must be greater that 0")
-			return
+		switch option {
+		case 1:
+			fmt.Println("Your balance is: ", accountBalance)
+		case 2:
+			fmt.Println("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. must be greater that 0")
+				return
+			}
+			accountBalance += depositAmount
+			fmt.Println("Your new balance is: ", accountBalance)
+		case 3:
+			fmt.Println("Your withdraw: ")
+			var amount float64
+			fmt.Scan(&amount)
+
+			if amount >= accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have.")
+				return
+			}
+			accountBalance -= amount
+			fmt.Println("Your new balance is: ", accountBalance)
+		case 4:
+			justALoopFunction()
+		default:
+			fmt.Println("Goodbye")
+			break
 		}
-		accountBalance += depositAmount
-		fmt.Println("Your new balance is: ", accountBalance)
-	} else if option == 3 {
-		fmt.Println("Your withdraw: ")
-		var amount float64
-		fmt.Scan(&amount)
-
-		if amount >= accountBalance {
-			fmt.Println("Invalid amount. You can't withdraw more than you have.")
-			return
-		}
-
-		accountBalance -= amount
-		fmt.Println("Your new balance is: ", accountBalance)
-	} else if option == 4 {
-		justALoopFunction()
-	} else {
-		fmt.Println("Goodbye")
-		return
 	}
 }
