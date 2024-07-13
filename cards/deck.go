461f3aa50ba2343d0b8c483daaa724263b15d54e
Francsco Garcia <packo.ive@gmail.com>
2024-07-13 15:40:30 -0600

    Receiver and custom type declarations

--- cards/deck.go
+++ cards/deck.go
@@ -5,11 +5,25 @@ import "fmt"
 // Create a new type of deck
 // Which is a slice of strings
 
+// Defining a deck: Is a slice of string
 type deck []string
 
-func newDeck() {
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
 }
 
+// This is a receiver
+// Where d is kind of ".this" or ".self" in javascript or python
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,34 @@
+// An executable package
 package main
 
+// Notes:
+
+/*
+*
+
+  - Run the script
+
+  - Open terminal in the root directory
+
+  - go run main.go deck.go
+
+    Go is not a POO language, so there's no classes concept inside it
+
+    Slices: Every element in a slice must
+    be of the same type.
+
+    cards := []string
+
+*
+*/
 func main() {
+	// Same ways of defining a string variable
 	//var card string = "Ace of Spades"
+
+	// Only for new variables
 	//card := newCard()
-	cards := deck{"Ace of diamonds", newCard()}
-	cards = append(cards, "Six of spades")
+
+	cards := newDeck()
 
 	cards.print()
 }
